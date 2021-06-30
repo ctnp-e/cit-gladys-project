4f99ca10ebb7b51e87b8a9c063913a854ad12c77
bluebuff3 <85905476+bluebuff3@users.noreply.github.com>
2021-06-29 17:28:30 -0700

    Lminhupdate

---
+++ GladysCompute.py
@@ -0,0 +1,13 @@
+import io
+
+import gladysSatellite as satellite
+
+"""
+	Student: Minh Le, Hubert Pham
+	Module: gladysCompute
+	Description: This module does compute program
+"""
+# start of code by Minh Le
+
+def gpsAverage(x, y):
+    """
