402ccd0027bc8cde56dc4c5a772f69097bd9481d
NgocNguyen2001 <85903967+NgocNguyen2001@users.noreply.github.com>
2021-06-30 23:01:53 -0700

    resolve conflicts

--- gladysCompute.py
+++ gladysCompute.py
@@ -3,28 +3,24 @@ import io
 import gladysSatellite as satellite
 
 """
-	Student: Gabriel Solomon
+	Student: Minh Le, Hubert Pham
 	Module: gladysCompute
-	Description: This module does …
+	Description: This module does compute program
 """
-
+# start of code by Minh Le
 
 def gpsAverage(x, y):
-	"""
-		document your function definition here. what does it do?
-	"""
+    """ 
+    This function computes the GPS value average of a position by taking a GPS value and dividing by 2 
+    """
+ 
+    value = satellite.gpsValue(x, y, "altitude")
 
-	"""
-		delete the remaining code *in this function* and replace it with
-		your own code. add more code to do what the assignment asks of you.
-	"""
+    average = value / 2
 
-	value = satellite.gpsValue(5, 6, "altitude")
+    return average
 
-	average = value / 2
-
-	return average
-
+# end of the code by Minh Le
 
 def distance(current, destination):
 	"""
