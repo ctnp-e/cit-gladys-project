0bb00a6582bfe2549248d2b80152dcc8f9974160
NgocNguyen2001 <85903967+NgocNguyen2001@users.noreply.github.com>
2021-07-01 16:10:09 -0700

    user login: support user login

--- gladysUserLogin.py
+++ gladysUserLogin.py
@@ -1,21 +1,18 @@
-import io
-
 """
-	Student: Gabriel Solomon
+	Student: Ngoc Nguyen
 	Module: gladysUserLogin
-	Description: This module does …
+	Description: This module does user login
 """
 
-
+# start of the codes by Ngoc Nguyen
 def login():
-	"""
-		document your function definition here. what does it do?
-	"""
+	# Loop until user enters a valid email address
+	isEmailValid = False
+	while not isEmailValid:
+		email = input('email: ')
+		isEmailValid = '@' in email
 
-	"""
-		delete the remaining code *in this function* and replace it with
-		your own code. add more code to do what the assignment asks of you.
-	"""
-	emailAddress = "malcomx@mecca.com"
+	password = input('password: ') # any password works
 
-	return emailAddress
+	return email
+# end of the codes by Ngoc Nguyen
