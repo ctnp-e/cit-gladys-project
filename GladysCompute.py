14cbf5c38dabff3e769121f2b5a187284e66593f
bluebuff3 <85905476+bluebuff3@users.noreply.github.com>
2021-06-30 19:22:11 -0700

    done

--- GladysCompute.py
+++ GladysCompute.py
@@ -10,4 +10,14 @@ import gladysSatellite as satellite
 # start of code by Minh Le
 
 def gpsAverage(x, y):
+    """ 
+    This function computes the GPS value average of a position by taking a GPS value and dividing by 2 
     """
+ 
+value = satellite.gpsValue(x, y, "altitude")
+
+average = value / 2
+
+return average
+
+# end of the code by Minh Le
